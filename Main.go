e711c4a0158d125712fb2bb2a4cd072af8c9535e
ArdianGorqaj <ardiang@uia.no>
2022-01-28 11:15:33 +0100

    first commit

---
+++ Main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+import "example.com/ardian/MyQuote"
+
+func main() {
+	fmt.Println(MyQuote.GetGlass())
+	fmt.Println(MyQuote.GetGo())
+	fmt.Println(MyQuote.GetOpt())
+	fmt.Println(MyQuote.GetHello())
+}
---
+++ MyQuote/Quotes.go
@@ -0,0 +1,23 @@
+package MyQuote
+
+import "rsc.io/quote"
+
+func GetGo() string {
+	return quote.Go()
+
+}
+
+func GetGlass() string {
+	return quote.Glass()
+
+}
+
+func GetHello() string {
+	return quote.Hello()
+
+}
+
+func GetOpt() string {
+	return quote.Opt()
+
+}
